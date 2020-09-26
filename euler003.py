47ce21960cc2c8b656f60ccba455e24453f53f9d
Rillis Vernucio <rillisnelson@gmail.com>
2020-09-26 06:42:03 -0300

    003

---
+++ euler003.py
@@ -0,0 +1,15 @@
+import sys
+
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    print(largest_prime_factor(n))
