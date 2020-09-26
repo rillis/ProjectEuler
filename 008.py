135ec539b4595b152d199613c7ff721bf0174e2c
Rillis Vernucio <rillisnelson@gmail.com>
2020-09-26 12:54:47 -0300

    008

---
+++ 008.py
@@ -0,0 +1,33 @@
+# 
+# ProjectEuler - HackerRank
+# Copyright (c) ProjectEuler - rillis. All rights reserved.
+# 
+# https://github.com/rillis/ProjectEuler
+# 
+
+import math
+import os
+import random
+import re
+import sys
+import itertools
+
+def produto(num):
+    if num.count("0")>0: return 0
+    m = 1
+    for x in num:
+        m*=int(x)
+    return m
+
+def solution(num, k):
+    a = [num[i:i+k] for i in range(len(num)-k+1)]
+    b = [produto(i) for i in a]
+    return max(b)
+
+if __name__ == "__main__":
+    t = int(input().strip())
+    for _ in range(t):
+        n,k = input().strip().split(' ')
+        n,k = [int(n),int(k)]
+        num = input().strip()
+        print(solution(num, k))
