1686dda33856fbefcc266317fc3a6bd7b771a935
Rillis Vernucio <rillisnelson@gmail.com>
2020-09-27 09:12:30 -0300

    010 fix

--- 010.py
+++ 010.py
@@ -1,5 +1,5 @@
 
-# Points X/100
+# Points 100/100
 # Notes: -
 
 # 
@@ -16,48 +16,22 @@ import re
 import sys
 import itertools
 
-'''
-def sieveEratosthenes(n):
-    primes = [2]
-    primes.extend(list(range(3, n+1, 2)))
-    for possiblePrime in primes:
-        #print(possiblePrime)
-        for x in primes[1:]:
-            if x%possiblePrime==0 and possiblePrime!=x:
-                primes.remove(x)
-    return primes
-'''
+def sieveEratosthenesSum(limit):
+    suml = [0] * limit
+    a = [True] * limit
+    a[0] = a[1] = False
+    for i, isprime in enumerate(a):
+        if isprime:
+            suml[i] = suml[i-1] + i
+            for n in range(i*i, limit, i):
+                a[n] = False
+        else:
+            suml[i] = suml[i-1]
+    return suml
 
-def sieveEratosthenes(n):
-    primes = [False, False, True]
-    primes.extend([True]*(n-2))
-
-    for num in range(3, n+1):
-        if num%2==1:
-            for n in range(3, len(primes), 2):
-                if num%n==0: 
-                    primes[num]=False
-                    break
-        else: primes[num]=False
-
-    print(len(primes))
-
-def solution(n, primes):
-    if len(primes)<1 or n<2: return 0
-    s = 0
-
-    #print(primes)
-
-    '''
-    if n>=(primes[-1]//2):
-        for x in range(len(primes)-1,-1,-1):
-            if primes[x]<=n:
-                return sum(primes[:x+1])
-    else:
-        for y in range(len(primes)):
-            if primes[y]>n:
-                return sum(primes[:y])
-    '''
+def solution(n, sumsSieve):
+    if n<2: return 0
+    return(sumsSieve[n])
 
 if __name__ == "__main__":
     t = int(input().strip())
@@ -66,7 +40,7 @@ if __name__ == "__main__":
     for _ in range(t):
         ns.append(int(input().strip()))
     
-    primes = sieveEratosthenes(max(ns))
-
+    sumsSieve = sieveEratosthenesSum(max(ns)+1)
+    
     for n in ns:
-        print(solution(n,primes))
+        print(solution(n, sumsSieve))
