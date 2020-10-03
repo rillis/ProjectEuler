38a9a36a3a3185fc3d550a20e27797935ca5122c
Rillis Vernucio <rillisnelson@gmail.com>
2020-10-03 11:48:49 -0300

    052

--- 0-100/052.py
+++ 0-100/052.py
@@ -20,10 +20,10 @@ import timeit
 def sameNumbers(a,b):
     return sorted(str(a))==sorted(str(b))
 
-def solution():
-    for i in range(1, 1000000):
+def solution(n, limit):
+    for i in range(1, limit+1):
         certo = True
-        for x in range(2, 7):
+        for x in range(2, n+1):
             if not sameNumbers(i, i*x):
                 certo = False
                 break
@@ -31,6 +31,6 @@ def solution():
 
 if __name__ == "__main__":
     start_t = timeit.default_timer()  # DEBUG
-    print(solution())
+    print(solution(6, 1000000))
     stop_t = timeit.default_timer()  # DEBUG
     print("TOTAL RUNTIME:", stop_t - start_t)  # DEBUG
